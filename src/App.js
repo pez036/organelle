31c1b122cdf7534931caab1ff6dc4fcee82c399a
Alex Zhang <alexzhang@macbook-pro.attlocal.net>
2020-11-01 18:17:40 -0800

    react default style

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-//import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
 
-      <h1>Welcome to organelle!</h1>
-      <a 
-        href="https://drive.google.com/drive/folders/1f90RqdXNojKlN-CAytjrkiAQ4xdT5yzP?usp=sharing">
-        Google Drive here </a>
-      {/*<header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+      
+      <header className="App-header">
+        <h1>Welcome to organelle!</h1>
+        <a 
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-  </header>*/}
+          href="https://drive.google.com/drive/folders/1f90RqdXNojKlN-CAytjrkiAQ4xdT5yzP?usp=sharing">
+        Google Drive here </a>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
     </div>
   );
 }
