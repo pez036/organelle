c3280254b27ccd5183f95e9e9b94c65dcf2705a1
Alex Zhang <alexzhang@macbook-pro.attlocal.net>
2020-11-01 21:23:13 -0800

    delete space

--- src/App.js
+++ src/App.js
@@ -3,15 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-      
+    <div className="App">    
       <header className="App-header">
         <h1>Welcome to organelle!</h1>
         <a 
           className="App-link"
           href="https://drive.google.com/drive/folders/1f90RqdXNojKlN-CAytjrkiAQ4xdT5yzP?usp=sharing">
-        Google Drive here </a>
+          Google Drive here </a>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
