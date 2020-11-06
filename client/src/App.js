f45e32bdffbd1787f751cd7527d026c5a5c7de17
Alex Zhang <alexzhang@macbook-pro.attlocal.net>
2020-11-06 00:42:46 -0800

    a

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import logo from './party_parrot.gif';
+import axios from 'axios';
+import './App.css';
+
+//test method, delete this later
+const handleTest = () => {
+  axios.get('http://localhost:5000/users')
+          .then(res => console.log(res.data.map(user => user.username)))
+          .catch(err => console.log('Err'+err))
+}
+
+function App() {
+  return (
+    <div className="App">    
+      <header className="App-header">
+        <h1>Welcome to organelle!</h1>
+        <button onClick={handleTest}>
+          test http get
+        </button>
+        <p>
+          Organelle helps your time management by integrating all <br/>
+          the course information in one App, and remind you when <br/>
+          due is immiment.
+        </p>
+        <a 
+          className="App-link"
+          href="https://drive.google.com/drive/folders/1f90RqdXNojKlN-CAytjrkiAQ4xdT5yzP?usp=sharing">
+          Google Drive here </a>
+        <img src={logo} alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.Port || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ORGANELLE_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('db connected');
+})
+
+const usersRouter = require('./routes/users');
+
+app.use('/users', usersRouter);
+
+app.listen(port, () => {
+    console.log('Server listening on port:', port);
+});
