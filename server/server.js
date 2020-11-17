72d9834129aecdfd9ee8782554c840988ab319ac
Alex Zhang <alexzhang@macbook-pro.attlocal.net>
2020-11-17 03:50:23 -0800

    port

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === "production") {
     });
 }
 
-const port = process.env.Port || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log('Server listening on port:', port);
 });
