1d4a2f84ca9ae3a17110b2de1f8a162be19a4915
Alex Zhang <alexzhang@macbook-pro.attlocal.net>
2020-11-17 02:40:12 -0800

    userRouter

--- server/routes/usersRouter.js
+++ server/routes/usersRouter.js
@@ -1,61 +1,111 @@
-const router = require('express').Router();
-let User = require('../models/user.model');
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const auth = require("../middleware/auth");
+const User = require("../models/userModel");
 
-router.route('/').get((req, res) => {
-    User.find()
-    .then(users => res.json(users))
-    .catch(err => res.status(400).json('Err'+err));
+router.post("/register", async (req, res) => {
+  try {
+    let { email, password, passwordCheck, displayName } = req.body;
+
+    // validate
+
+    if (!email || !password || !passwordCheck)
+      return res.status(400).json({ msg: "Not all fields have been entered." });
+    if (password.length < 5)
+      return res
+        .status(400)
+        .json({ msg: "The password needs to be at least 5 characters long." });
+    if (password !== passwordCheck)
+      return res
+        .status(400)
+        .json({ msg: "Enter the same password twice for verification." });
+
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser)
+      return res
+        .status(400)
+        .json({ msg: "An account with this email already exists." });
+    if (!displayName) displayName = email;
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      email,
+      password: passwordHash,
+      displayName,
+    });
+    const savedUser = await newUser.save();
+    res.json(savedUser);
+    console.log("registered");
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
-router.route('/register').post((req, res) => {
-    const username = req.body.username;
-    const email = req.body.email;
-    const courses = req.body.courses;
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-    const newUser = new User({username, email, courses});
-    newUser.save()
-        .then(() => res.json('User added!'))
-        .catch(err => res.status(400).json('Err'+err));
+    // validate
+    if (!email || !password)
+      return res.status(400).json({ msg: "Not all fields have been entered." });
+
+    const user = await User.findOne({ email: email });
+    if (!user)
+      return res
+        .status(400)
+        .json({ msg: "No account with this email has been registered." });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials." });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.json({
+      token,
+      user: {
+        id: user._id,
+        displayName: user.displayName,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
-router.route('/:id').get((req, res) => {
-    User.findById(req.params.id)
-    .then(user => res.json(user))
-    .catch(err => res.status(400).json('Err'+err));
+router.delete("/delete", auth, async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.user);
+    res.json(deletedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
-router.route('/:id').delete((req, res) => {
-    User.findByIdAndDelete(req.params.id)
-    .then(() => res.json('User deleted'))
-    .catch(err => res.status(400).json('Err'+err));
+router.post("/tokenIsValid", async (req, res) => {
+  try {
+    const token = req.header("x-auth-token");
+    if (!token) return res.json(false);
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    if (!verified) return res.json(false);
+
+    const user = await User.findById(verified.id);
+    if (!user) return res.json(false);
+
+    return res.json(true);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
-//TODO, not efficient, can improve with update()
-router.route('/update/:id').post((req, res) => {
-    User.findById(req.params.id)
-    .then(user => {
-        user.username = req.body.username.length > 0 ? req.body.username : user.username ;
-        user.email = req.body.email.length > 0 ? req.body.email : user.email;
-
-        user.save()
-        .then(() => res.json('user updated'))
-        .catch(err => res.status(400).json('Err'+err));
-    })
-    .catch(err => res.status(400).json('Err'+err));
-});
-
-router.route('/updateusername/:id').post((req, res) => {
-    User.update({"_id": req.params.id},
-        {"username": req.body.username})
-    .then(() => res.json('username updated'))
-    .catch(err => res.status(400).json('Err'+err));
-});
-
-router.route('/updateemail/:id').post((req, res) => {
-    User.update({"_id": req.params.id},
-        {"email": req.body.email})
-    .then(() => res.json('email updated'))
-    .catch(err => res.status(400).json('Err'+err));
+router.get("/", auth, async (req, res) => {
+  const user = await User.findById(req.user);
+  res.json({
+    displayName: user.displayName,
+    id: user._id,
+  });
 });
 
 module.exports = router;
