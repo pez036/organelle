1e590c3d7f5935873ede5af0c4f4b2e53231f0ba
Alex Zhang <alexzhang@macbook-pro.attlocal.net>
2020-11-18 21:21:24 -0800

    message

--- server/server.js
+++ server/server.js
@@ -28,6 +28,7 @@ if (process.env.NODE_ENV === "production") {
 
     // Set static folder
     // All the javascript and css files will be read and served from this folder
+    //this is a new comment
     app.use(express.static("client/build"));
   
     // index.html for all page routes  html or routing and naviagtion
