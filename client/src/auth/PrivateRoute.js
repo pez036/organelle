2ec7fa3f25bd3afd36b96096a86f6664d8d8e69c
Alex Zhang <alexzhang@macbook-pro.attlocal.net>
2020-12-02 19:22:49 -0800

    private Route definition

--- client/src/auth/PrivateRoute.js
+++ client/src/auth/PrivateRoute.js
@@ -1,15 +1,14 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import UserContext from '../context/UserContext';
+
 
 const PrivateRoute = ({component: Component, ...rest}) => {
-    const {userData} = useContext(UserContext);
     return (
 
         // Show the component only when the user is logged in
         // Otherwise, redirect the user to /signin page
         <Route {...rest} render={props => (
-            userData.user ?
+            localStorage.getItem("auth-token") !== "" ?
                 <Component {...props} />
             : <Redirect to="/login" />
         )} />
