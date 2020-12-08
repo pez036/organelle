301d7091cbe43cc369fd3eac46b467d6d47a019e
Si Thu Kaung Set <skaungse@ucsd.edu>
2020-12-07 16:24:48 -0800

    Email send

--- server/routes/eventRouter.js
+++ server/routes/eventRouter.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const auth = require("../middleware/auth");
 const Event = require("../models/eventModel");
-
+const nodemailer = require("nodemailer");
+const moment = require("moment");
 router.post("/add",auth,async (req, res) => {
     try{
         let {title, type, startTime, endTime, priority, description, courseName, canvasID} = req.body;
@@ -104,5 +105,63 @@ router.delete('/:id',  auth, async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 })
+router.post("/emailstart",auth, async (req, res) => {
+    try{
+        console.log("HERE");
+        // var emailTask = cron.schedule('* * * * *', () => {
+            console.log(req.body)
+            const event1 = await Event.find({startTime: moment().startOf("day"), userID: req.user});
+            const event2 = await Event.find({startTime: moment().add(1, "day").startOf("day"), userID: req.user});
+            const event3 = await Event.find({startTime: moment().add(2, "day").startOf("day"), userID: req.user});
+            console.log(event1);
+            var transporter = nodemailer.createTransport({
+                service: 'Gmail',
+                auth: {
+                    user: 'organelleplanner@gmail.com',
+                    pass: 'organelle110'
+                }
+            });
+            //send email only if events exist
+            let message = "Hope you're having a fantastic day! Here are your deadline reminders for the next three days: \n\n";
+            if(event1.length != 0){
+                event1.forEach(e => message = message + e.title + "due at " + e.endTime + "\n");
+                console.log(message);
+            }
+            else{
+                message = message + "\nHorray! You have nothing due today!\n\n";
+            }
+            console.log(message)
+            if(event2.length != 0){
+                event2.forEach(e => message = message + e.title + "due at " + e.endTime + "\n");
+                console.log(message);
+            }
+            else{
+                message = message + "\nHorray! You have nothing due tomorrow!\n\n";
+            }
+            if(event3.length != 0){
+                event3.forEach(e => message = message + e.title + "due at " + e.endTime + "\n");
+                console.log(message);
+            }
+            else{
+                message = message + "\nHorray! You have nothing due two days from now!\n\n";
+            }
+            
+            
+            transporter.sendMail({
+            from: 'organelleplanner',
+                to: 'mhtoo@ucsd.edu',
+                subject: 'Deadline Reminder',
+                text: message
+            });
+        // });
+        // emailTask.start();
+    }
+    catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+})
+
+
+
 
 module.exports = router;
